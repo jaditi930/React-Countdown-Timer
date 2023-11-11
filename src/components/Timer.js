f4b9e3b3245f01b7b431468231f09dc93028831d
Aditi Jain <jaditi930@gmail.com>
2023-11-12 00:42:37 +0530

    states defined

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,18 +1,45 @@
+import {useState} from 'react'
+
 export default function Timer(){
-    let padding={padding:"40px 45px",fontSize:"3rem"}
-    let margin={margin:"40px 20px",padding:"10px",borderRadius:"30px"}
+    let padding={padding:"40px 45px",fontSize:"3rem",width:"100px"}
+    let margin={margin:"40px 35px",padding:"10px"}
     let container={minHeight:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}
+
+    const [seconds,setSecs]=useState("00");
+    const [minutes,setMins]=useState("00");
+    const [hours,setHours]=useState("00");
+
+
     return (
         
         <div style={container}>
             <div>
 
             <div style={{marginLeft:"15px"}}>
-                <span style={padding}>00</span>
+                <input type="number" style={padding} value={hours} min={0} max={23} 
+                onChange={(e)=>{
+                    let hrs=parseInt(e.target.value)
+                    if(hrs<10)
+                    hrs="0"+hrs
+                    setHours(hrs)
+                }}/>
                 <span style={padding}>:</span>
-                <span style={padding}>00</span>
+                <input type="number" style={padding} value={minutes} min={0} max={59} 
+                onChange={(e)=>{
+                let mins=parseInt(e.target.value)
+                if(mins<10)
+                mins="0"+mins
+                setMins(mins)
+                }}
+                />
                 <span style={padding}>:</span>
-                <span style={padding}>00</span>
+                <input type="number" style={padding} value={seconds} min={0} max={59} 
+                onChange={(e)=>{
+                let secs=parseInt(e.target.value)
+                if(secs<10)
+                secs="0"+secs
+                setSecs(secs)
+                }}/>
             </div>
 
             <div>
